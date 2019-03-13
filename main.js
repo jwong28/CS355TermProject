8b4f32497a57d584d127e32c72d55ba8f99930e9
Simon <simonhe1@mail.com>
2019-03-13 12:09:11 -0400

    Updated browser page

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
     width.innerHTML = "Browser Width: " + window.outerWidth;
     document.getElementById("screenHeight").innerHTML = "Screen Height: " + screen.availHeight;
     document.getElementById("screenWidth").innerHTML = "Screen Width: " + screen.availWidth;
+    document.getElementById("pageLocation").innerHTML = "Page Location: " + window.location.href;
+    document.getElementById("pageHostName").innerHTML = "Page Host Name: " + window.location.hostname;
+    document.getElementById("pagePathName").innerHTML = "Page Path Name: " + window.location.pathname;
+    document.getElementById("pageProtocol").innerHTML = "Page Protocol: " + window.location.protocol;
+    document.getElementById("pagePort").innerHTML = "Page Port Number: " + window.location.port;
 });
 
 //Function for having an interactive feel whenever browser is resized
